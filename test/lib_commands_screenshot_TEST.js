219a4d3ebd750c9eb13cba1f0b4c1ee3ce066d6e
asciidisco <public@asciidisco.com>
2013-10-15 17:02:31 +0200

    Initial commit

---
+++ test/lib_commands_screenshot_TEST.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Screenshot = require('../lib/commands/screenshot.js');
+
+describe('dalek-driver-sauce Commands Screenshot', function() {
+
+  it('i should think of proper testing', function(){
+    expect(Screenshot).to.be.ok;
+  });
+
+});
